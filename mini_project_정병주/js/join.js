f5e34be8562f672dd16b8cddf917854e0ac3a17f
ChocoJ1721031 <qudwn980226@naver.com>
2022-11-12 07:31:22 +0900

    join.js - input박스 대응 텍스트 작업
    회원가입 입력란 내용에 따른 반응형 텍스트 작업.

---
+++ mini_project_정병주/js/join.js
@@ -0,0 +1,104 @@
+var regExp1 = /[~!\@\#$%^&*\()\-=+_'\;<>0-9\/.\`:\"\\,\[\]?|{}]/gi;
+
+function id_script(el, minlength, maxlength) {
+    // if(regExp1.test(el.value)) {
+    //     el.value = el.value.substring( 0 , el.value.length - 1 );
+    // } else {
+        if(el.value.length > maxlength || el.value.length < minlength) {
+            document.getElementById('input_id_p').innerHTML = "6자 이상 16자 이하의 영문 혹은 영문과 숫자를 조합";
+            document.getElementById('input_id_p').style.display = "flex";
+        } else {
+            document.getElementById('input_id_p').innerHTML = "";
+            document.getElementById('input_id_p').style.display = "none";
+        }
+    //}
+    
+}
+
+var regExp2 = /[\s]/gi;
+function input_pw_script(el, minlength) {
+    if(el.value.length < 10) {
+        document.getElementById('input_pw_p').innerHTML = "최소 10자 이상 입력";
+        document.getElementById('input_pw_p').style.display = "flex";
+    } else if(el.value.length >= 10 && (!regExp2.test(el.value) || (/[a-zA-Z]/gi.test(el.value) && /[0-9]/gi.test(el.value)) || (/[a-zA-Z]/gi.test(el.value) && regExp1.test(el.value))|| (regExp1.test(el.value) && /[0-9]/gi.test(el.value)))) {
+        document.getElementById('input_pw_p').innerHTML = "";
+        document.getElementById('input_pw_p').style.display = "none";
+    } else {
+        document.getElementById('input_pw_p').innerHTML = "영문/숫자/특수문자(공백 제외)만 허용하며, 2개 이상 조합";
+        document.getElementById('input_pw_p').style.display = "flex";
+    }
+}
+
+function input_pw_check_script(el) {
+    if(el.value == "") {
+        document.getElementById('input_pw_check_p').innerHTML = "비밀번호를 한번 더 입력해 주세요.";
+        document.getElementById('inpinput_pw_check_p').style.display = "flex";
+        
+    } else if(el.value != document.getElementById('input_pw').value) {
+        document.getElementById('input_pw_check_p').innerHTML = "동일한 비밀번호를 입력";
+        document.getElementById('input_pw_check_p').style.display = "flex";
+    } else {
+        document.getElementById('input_pw_check_p').innerHTML = "";
+        document.getElementById('input_pw_check_p').style.display = "none";
+    }
+}
+
+function input_user_name_script(el) {
+    if(el.value == "") {
+        document.getElementById('input_user_name_p').innerHTML = "이름을 입력해 주세요.";
+        document.getElementById('input_user_name_p').style.display = "flex";
+    } else {
+        document.getElementById('input_user_name_p').innerHTML = "";
+        document.getElementById('input_user_name_p').style.display = "none";
+    }
+}
+
+var regExp3 = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]/gi;
+function input_mail_script(el) {
+    if(el.value == "") {
+        document.getElementById('input_mail_p').innerHTML = "이메일을 입력해 주세요.";
+        document.getElementById('input_mail_p').style.display = "flex";
+    } else if(regExp3.test(el.value)) {
+        document.getElementById('input_mail_p').innerHTML = "이메일 형식으로 입력해 주세요.";
+        document.getElementById('input_mail_p').style.display = "none";
+    } else {
+        document.getElementById('input_mail_p').innerHTML = "";
+        document.getElementById('input_mail_p').style.display = "none";
+    }
+}
+
+var regExp4 = /[a-zA-Zㄱ-ㅎ가-힣]/gi
+function input_phone_script(el) {
+    el.value = el.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+    if(el.value == "") {
+        document.getElementById('input_phone_p').innerHTML = "휴대폰 번호를 입력해 주세요.";
+        document.getElementById('input_phone_p').style.display = "flex";
+    } else {
+        document.getElementById('input_phone_p').innerHTML = "";
+        document.getElementById('input_phone_p').style.display = "none";
+    }
+}
+
+let today = new Date();
+var fullYear = today.getFullYear();
+function input_birth_script() {
+    document.getElementById('input_birth_year').value
+    = document.getElementById('input_birth_year').value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+    document.getElementById('input_birth_month').value
+    = document.getElementById('input_birth_month').value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+    document.getElementById('input_birth_date').value
+    = document.getElementById('input_birth_date').value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+    if(document.getElementById('input_birth_year').value == "" || fullYear - document.getElementById('input_birth_year').value > 100 || document.getElementById('input_birth_year').value.length < 4) {
+        document.getElementById('input_birth_p').innerHTML = "태어난 년도 4자리를 정확하게 입력해주세요.";
+        document.getElementById('input_birth_p').style.display = "flex";
+    } else if(document.getElementById('input_birth_month').value == "" || document.getElementById('input_birth_month').value > 12 || document.getElementById('input_birth_month').value == 0) {
+        document.getElementById('input_birth_p').innerHTML = "태어난 월을 정확하게 입력해주세요.";
+        document.getElementById('input_birth_p').style.display = "flex";
+    } else if(document.getElementById('input_birth_date').value == "" || document.getElementById('input_birth_date').value > 31 || document.getElementById('input_birth_date').value == 0) {
+        document.getElementById('input_birth_p').innerHTML = "태어난 일을 정확하게 입력해주세요.";
+        document.getElementById('input_birth_p').style.display = "flex";
+    } else {
+        document.getElementById('input_birth_p').innerHTML = "";
+        document.getElementById('input_birth_p').style.display = "none";
+    }
+}
