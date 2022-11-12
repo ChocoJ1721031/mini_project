c6d608e3452f60ea425a68bcada974cb80b89603
homin0203 <kwonhomin317@gmail.com>
2022-11-12 19:45:16 +0900

    list_action.js 수정
    2022.11.12
    목록이 끝나도 클릭 시 넘어가고 우측 버튼이 사라지지 않는 부분 수정

--- mini_project_강롱기/index_section추가/js/list_action.js
+++ mini_project_강롱기/index_section추가/js/list_action.js
@@ -1,17 +1,23 @@
 window.onload = function() {
     var a = 0;
     document.getElementById("insta_sButton").onclick = function () {
-        document.getElementById("insta_fButton").style.display = "inline-block";
         a += -1068;
         var t_value = "translate3d(" + a +"px, 0px, 0px)";
         document.getElementsByClassName("m_s_3_list_swiper_wrap")[0].style.transform = t_value;
+
+        document.getElementById("insta_fButton").style.display = "inline-block";
+        if(a == -1068*2) {
+            document.getElementById("insta_sButton").style.display = "none";
+        }
     }
     document.getElementById("insta_fButton").onclick = function () {
         a += +1068;
+        var t_value = "translate3d(" + a +"px, 0px, 0px)";
+        document.getElementsByClassName("m_s_3_list_swiper_wrap")[0].style.transform = t_value;
+
+        document.getElementById("insta_sButton").style.display = "inline-block";
         if(a == 0) {
             document.getElementById("insta_fButton").style.display = "none";
         }
-        var t_value = "translate3d(" + a +"px, 0px, 0px)";
-        document.getElementsByClassName("m_s_3_list_swiper_wrap")[0].style.transform = t_value;
     }
 }
