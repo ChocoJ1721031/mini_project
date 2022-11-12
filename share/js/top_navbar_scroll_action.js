3674b8a8da283e96b669bc42dc6ae19efa83803c
homin0203 <kwonhomin317@gmail.com>
2022-11-12 14:22:54 +0900

    section nav
    section 공통 nav 구현
    html - nav(#right_nav) 추가
    css - #right_nav 및 하위class 관련 코드 추가
    js - nav의 scroll_action관련 추가

--- share/js/top_navbar_scroll_action.js
+++ share/js/top_navbar_scroll_action.js
@@ -9,4 +9,15 @@ function nav_fix() {
         document.getElementById('h_second').style.position = "relative";
         document.getElementById('section').style.margin = "0";
     }
+
+    for(var b = 80; b < window.innerHeight; b+=80) {
+        if(document.documentElement.scrollTop > 198+b) {
+            var c = document.documentElement.scrollTop - 198 + 714;
+            var d = c+"px";
+            document.getElementById('right_nav').style.top = d;
+        }
+    }
+    if(document.documentElement.scrollTop <= 198) {
+        document.getElementById('right_nav').style.top = "714px";
+    }
 }
