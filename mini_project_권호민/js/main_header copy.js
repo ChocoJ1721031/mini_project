647a07d6b88284150c2bf69f18be9f92e916123b
homin0203 <kwonhomin317@gmail.com>
2022-11-13 21:41:37 +0900

    header category 부분 hover 수정
    hover 관련 css 수정

---
+++ mini_project_권호민/js/main_header copy.js
@@ -0,0 +1,87 @@
+window.onload = function () {
+        document.getElementsByClassName("s_c_list_li")[0].addEventListener("mouseover",over0);
+        document.getElementsByClassName("s_c_list_li")[1].addEventListener("mouseover",over1);
+        document.getElementsByClassName("s_c_list_li")[2].addEventListener("mouseover",over2);
+        document.getElementsByClassName("s_c_list_li")[3].addEventListener("mouseover",over3);
+        document.getElementsByClassName("s_c_list_li")[4].addEventListener("mouseover",over4);
+        document.getElementsByClassName("s_c_list_li")[5].addEventListener("mouseover",over5);
+        
+        document.getElementsByClassName("s_c_list_li")[0].addEventListener("mouseout",out0);
+        document.getElementsByClassName("s_c_list_li")[1].addEventListener("mouseout",out1);
+        document.getElementsByClassName("s_c_list_li")[2].addEventListener("mouseout",out2);
+        document.getElementsByClassName("s_c_list_li")[3].addEventListener("mouseout",out3);
+        document.getElementsByClassName("s_c_list_li")[4].addEventListener("mouseout",out4);
+        document.getElementsByClassName("s_c_list_li")[5].addEventListener("mouseout",out5);
+}
+// var document.getElementsByClassName("s_c_list_li") = document.getElementById("s_c_list").getElementsByClassName("document.getElementsByClassName("s_c_list_li")");
+function over0() {
+    document.getElementsByClassName("s_c_list_li")[0].getElementsByTagName("ul")[0].style.left = "255px";
+    document.getElementsByClassName("s_c_list_li")[0].getElementsByTagName("ul")[0].style.display = "block";
+    document.getElementsByClassName("s_c_list_li")[0].style.backgroundColor = "rgb(247, 247, 247)";
+}
+
+function over1() {
+    document.getElementsByClassName("s_c_list_li")[1].getElementsByTagName("ul")[0].style.left = "255px";
+    document.getElementsByClassName("s_c_list_li")[1].getElementsByTagName("ul")[0].style.display = "block";
+    document.getElementsByClassName("s_c_list_li")[1].style.backgroundColor = "rgb(247, 247, 247)";
+}
+
+function over2() {
+    document.getElementsByClassName("s_c_list_li")[2].getElementsByTagName("ul")[0].style.left = "255px";
+    document.getElementsByClassName("s_c_list_li")[2].getElementsByTagName("ul")[0].style.display = "block";
+    document.getElementsByClassName("s_c_list_li")[2].style.backgroundColor = "rgb(247, 247, 247)";
+}
+
+function over3() {
+    document.getElementsByClassName("s_c_list_li")[3].getElementsByTagName("ul")[0].style.left = "255px";
+    document.getElementsByClassName("s_c_list_li")[3].getElementsByTagName("ul")[0].style.display = "block";
+    document.getElementsByClassName("s_c_list_li")[3].style.backgroundColor = "rgb(247, 247, 247)";
+}
+
+function over4() {
+    document.getElementsByClassName("s_c_list_li")[4].getElementsByTagName("ul")[0].style.left = "255px";
+    document.getElementsByClassName("s_c_list_li")[4].getElementsByTagName("ul")[0].style.display = "block";
+    document.getElementsByClassName("s_c_list_li")[4].style.backgroundColor = "rgb(247, 247, 247)";
+}
+
+function over5() {
+    document.getElementsByClassName("s_c_list_li")[5].getElementsByTagName("ul")[0].style.left = "255px";
+    document.getElementsByClassName("s_c_list_li")[5].getElementsByTagName("ul")[0].style.display = "block";
+    document.getElementsByClassName("s_c_list_li")[5].style.backgroundColor = "rgb(247, 247, 247)";
+}
+function out0() {
+    document.getElementsByClassName("s_c_list_li")[0].getElementsByTagName("ul")[0].style.left = "0px";
+    document.getElementsByClassName("s_c_list_li")[0].getElementsByTagName("ul")[0].style.display = "none";
+    document.getElementsByClassName("s_c_list_li")[0].style.backgroundColor = "rgb(255, 255, 255)";
+}
+
+function out1() {
+    document.getElementsByClassName("s_c_list_li")[1].getElementsByTagName("ul")[0].style.left = "0px";
+    document.getElementsByClassName("s_c_list_li")[1].getElementsByTagName("ul")[0].style.display = "none";
+    document.getElementsByClassName("s_c_list_li")[1].style.backgroundColor = "rgb(255, 255, 255)";
+}
+
+function out2() {
+    document.getElementsByClassName("s_c_list_li")[2].getElementsByTagName("ul")[0].style.left = "0px";
+    document.getElementsByClassName("s_c_list_li")[2].getElementsByTagName("ul")[0].style.display = "none";
+    document.getElementsByClassName("s_c_list_li")[2].style.backgroundColor = "rgb(255, 255, 255)";
+}
+
+function out3() {
+    document.getElementsByClassName("s_c_list_li")[3].getElementsByTagName("ul")[0].style.left = "0px";
+    document.getElementsByClassName("s_c_list_li")[3].getElementsByTagName("ul")[0].style.display = "none";
+    document.getElementsByClassName("s_c_list_li")[3].style.backgroundColor = "rgb(255, 255, 255)";
+}
+
+function out4() {
+    document.getElementsByClassName("s_c_list_li")[4].getElementsByTagName("ul")[0].style.left = "0px";
+    document.getElementsByClassName("s_c_list_li")[4].getElementsByTagName("ul")[0].style.display = "none";
+    document.getElementsByClassName("s_c_list_li")[4].style.backgroundColor = "rgb(255, 255, 255)";
+}
+
+function out5() {
+    document.getElementsByClassName("s_c_list_li")[5].getElementsByTagName("ul")[0].style.left = "0px";
+    document.getElementsByClassName("s_c_list_li")[5].getElementsByTagName("ul")[0].style.display = "none";
+    document.getElementsByClassName("s_c_list_li")[5].style.backgroundColor = "rgb(255, 255, 255)";
+}
+
