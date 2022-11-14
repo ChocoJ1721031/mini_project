38f89f28d3d13d30d44ed252a7dec3658a10714e
homin0203 <kwonhomin317@gmail.com>
2022-11-14 19:19:14 +0900

    메인 section 백업파일 수정 업로드
    .

--- mini_project_권호민/index_section추가/js/list_action.js
+++ mini_project_권호민/index_section추가/js/list_action.js
@@ -20,4 +20,88 @@ window.onload = function() {
             document.getElementById("insta_fButton").style.display = "none";
         }
     }
+    var b = 0;
+    document.getElementById("m_s_1_list_button_right").onclick = function () {
+        b += -1068;
+        var t_value = "translate3d(" + b +"px, 0px, 0px)";
+        document.getElementsByClassName("m_s_1_list_swiper_wrap")[0].style.transform = t_value;
+
+        document.getElementById("m_s_1_list_button_left").style.display = "inline-block";
+        if(b == -1068*2) {
+            document.getElementById("m_s_1_list_button_right").style.display = "none";
+        }
+    }
+    document.getElementById("m_s_1_list_button_left").onclick = function () {
+        b += +1068;
+        var t_value = "translate3d(" + b +"px, 0px, 0px)";
+        document.getElementsByClassName("m_s_1_list_swiper_wrap")[0].style.transform = t_value;
+
+        document.getElementById("m_s_1_list_button_right").style.display = "inline-block";
+        if(b == 0) {
+            document.getElementById("m_s_1_list_button_left").style.display = "none";
+        }
+    }
+    var c = 0;
+    document.getElementById("m_s_4_list_button_right").onclick = function () {
+        c += -1068;
+        var t_value = "translate3d(" + c +"px, 0px, 0px)";
+        document.getElementsByClassName("m_s_4_list_swiper_wrap")[0].style.transform = t_value;
+
+        document.getElementById("m_s_4_list_button_left").style.display = "inline-block";
+        if(c == -1068*2) {
+            document.getElementById("m_s_4_list_button_right").style.display = "none";
+        }
+    }
+    document.getElementById("m_s_4_list_button_left").onclick = function () {
+        c += +1068;
+        var t_value = "translate3d(" + c +"px, 0px, 0px)";
+        document.getElementsByClassName("m_s_4_list_swiper_wrap")[0].style.transform = t_value;
+
+        document.getElementById("m_s_4_list_button_right").style.display = "inline-block";
+        if(c == 0) {
+            document.getElementById("m_s_4_list_button_left").style.display = "none";
+        }
+    }
+    var d = 0;
+    document.getElementById("m_s_5_list_button_right").onclick = function () {
+        d += -1068;
+        var t_value = "translate3d(" + d +"px, 0px, 0px)";
+        document.getElementsByClassName("m_s_5_list_swiper_wrap")[0].style.transform = t_value;
+
+        document.getElementById("m_s_5_list_button_left").style.display = "inline-block";
+        if(d == -1068*2) {
+            document.getElementById("m_s_5_list_button_right").style.display = "none";
+        }
+    }
+    document.getElementById("m_s_5_list_button_left").onclick = function () {
+        d += +1068;
+        var t_value = "translate3d(" + d +"px, 0px, 0px)";
+        document.getElementsByClassName("m_s_5_list_swiper_wrap")[0].style.transform = t_value;
+
+        document.getElementById("m_s_5_list_button_right").style.display = "inline-block";
+        if(d == 0) {
+            document.getElementById("m_s_5_list_button_left").style.display = "none";
+        }
+    }
+    var e = 0;
+    document.getElementById("m_s_2_list_button_right").onclick = function () {
+        e += -1098;
+        var t_value = "translate3d(" + e +"px, 0px, 0px)";
+        document.getElementsByClassName("m_s_2_list_swiper_wrap")[0].style.transform = t_value;
+
+        document.getElementById("m_s_2_list_button_left").style.display = "inline-block";
+        if(e == -1098) {
+            document.getElementById("m_s_2_list_button_right").style.display = "none";
+        }
+    }
+    document.getElementById("m_s_2_list_button_left").onclick = function () {
+        e += +1098;
+        var t_value = "translate3d(" + e +"px, 0px, 0px)";
+        document.getElementsByClassName("m_s_2_list_swiper_wrap")[0].style.transform = t_value;
+
+        document.getElementById("m_s_2_list_button_right").style.display = "inline-block";
+        if(e == 0) {
+            document.getElementById("m_s_2_list_button_left").style.display = "none";
+        }
+    }
 }
