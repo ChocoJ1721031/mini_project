f7b58973d137a03989c165a713f3564932c7714a
homin0203 <kwonhomin317@gmail.com>
2022-11-14 21:53:46 +0900

    main_header.js 수정
    2022.11.14
    javascript로 구현된 클릭이벤트 jQuery로 수정
    및 index.html에 관련 태그 id명 class명으로 수정

--- mini_project_권호민/js/main_header copy.js
+++ mini_project_권호민/js/main_header copy.js
@@ -1,87 +1,57 @@
-window.onload = function () {
-        document.getElementsByClassName("s_c_list_li")[0].addEventListener("mouseover",over0);
-        document.getElementsByClassName("s_c_list_li")[1].addEventListener("mouseover",over1);
-        document.getElementsByClassName("s_c_list_li")[2].addEventListener("mouseover",over2);
-        document.getElementsByClassName("s_c_list_li")[3].addEventListener("mouseover",over3);
-        document.getElementsByClassName("s_c_list_li")[4].addEventListener("mouseover",over4);
-        document.getElementsByClassName("s_c_list_li")[5].addEventListener("mouseover",over5);
-        
-        document.getElementsByClassName("s_c_list_li")[0].addEventListener("mouseout",out0);
-        document.getElementsByClassName("s_c_list_li")[1].addEventListener("mouseout",out1);
-        document.getElementsByClassName("s_c_list_li")[2].addEventListener("mouseout",out2);
-        document.getElementsByClassName("s_c_list_li")[3].addEventListener("mouseout",out3);
-        document.getElementsByClassName("s_c_list_li")[4].addEventListener("mouseout",out4);
-        document.getElementsByClassName("s_c_list_li")[5].addEventListener("mouseout",out5);
-}
-// var document.getElementsByClassName("s_c_list_li") = document.getElementById("s_c_list").getElementsByClassName("document.getElementsByClassName("s_c_list_li")");
-function over0() {
-    document.getElementsByClassName("s_c_list_li")[0].getElementsByTagName("ul")[0].style.left = "255px";
-    document.getElementsByClassName("s_c_list_li")[0].getElementsByTagName("ul")[0].style.display = "block";
-    document.getElementsByClassName("s_c_list_li")[0].style.backgroundColor = "rgb(247, 247, 247)";
-}
+$(function() {
+    $('.s_click').click(function() {
+        $('.s_click').css("color", "black");
+        $(this).css("color", "rgb(95, 0, 128)");
+    })
+});
 
-function over1() {
-    document.getElementsByClassName("s_c_list_li")[1].getElementsByTagName("ul")[0].style.left = "255px";
-    document.getElementsByClassName("s_c_list_li")[1].getElementsByTagName("ul")[0].style.display = "block";
-    document.getElementsByClassName("s_c_list_li")[1].style.backgroundColor = "rgb(247, 247, 247)";
-}
+// window.onload = function () {
+//     for(var i = 0; i < 4; i++) {
+//         var id = "s_click"+i;
+//         var f = "c" + i;
+//         var f_name = f + "();"
+//         document.getElementById(id).setAttribute("onclick",f_name);
+//     }
+// }
 
-function over2() {
-    document.getElementsByClassName("s_c_list_li")[2].getElementsByTagName("ul")[0].style.left = "255px";
-    document.getElementsByClassName("s_c_list_li")[2].getElementsByTagName("ul")[0].style.display = "block";
-    document.getElementsByClassName("s_c_list_li")[2].style.backgroundColor = "rgb(247, 247, 247)";
-}
-
-function over3() {
-    document.getElementsByClassName("s_c_list_li")[3].getElementsByTagName("ul")[0].style.left = "255px";
-    document.getElementsByClassName("s_c_list_li")[3].getElementsByTagName("ul")[0].style.display = "block";
-    document.getElementsByClassName("s_c_list_li")[3].style.backgroundColor = "rgb(247, 247, 247)";
-}
-
-function over4() {
-    document.getElementsByClassName("s_c_list_li")[4].getElementsByTagName("ul")[0].style.left = "255px";
-    document.getElementsByClassName("s_c_list_li")[4].getElementsByTagName("ul")[0].style.display = "block";
-    document.getElementsByClassName("s_c_list_li")[4].style.backgroundColor = "rgb(247, 247, 247)";
-}
-
-function over5() {
-    document.getElementsByClassName("s_c_list_li")[5].getElementsByTagName("ul")[0].style.left = "255px";
-    document.getElementsByClassName("s_c_list_li")[5].getElementsByTagName("ul")[0].style.display = "block";
-    document.getElementsByClassName("s_c_list_li")[5].style.backgroundColor = "rgb(247, 247, 247)";
-}
-function out0() {
-    document.getElementsByClassName("s_c_list_li")[0].getElementsByTagName("ul")[0].style.left = "0px";
-    document.getElementsByClassName("s_c_list_li")[0].getElementsByTagName("ul")[0].style.display = "none";
-    document.getElementsByClassName("s_c_list_li")[0].style.backgroundColor = "rgb(255, 255, 255)";
-}
-
-function out1() {
-    document.getElementsByClassName("s_c_list_li")[1].getElementsByTagName("ul")[0].style.left = "0px";
-    document.getElementsByClassName("s_c_list_li")[1].getElementsByTagName("ul")[0].style.display = "none";
-    document.getElementsByClassName("s_c_list_li")[1].style.backgroundColor = "rgb(255, 255, 255)";
-}
-
-function out2() {
-    document.getElementsByClassName("s_c_list_li")[2].getElementsByTagName("ul")[0].style.left = "0px";
-    document.getElementsByClassName("s_c_list_li")[2].getElementsByTagName("ul")[0].style.display = "none";
-    document.getElementsByClassName("s_c_list_li")[2].style.backgroundColor = "rgb(255, 255, 255)";
-}
-
-function out3() {
-    document.getElementsByClassName("s_c_list_li")[3].getElementsByTagName("ul")[0].style.left = "0px";
-    document.getElementsByClassName("s_c_list_li")[3].getElementsByTagName("ul")[0].style.display = "none";
-    document.getElementsByClassName("s_c_list_li")[3].style.backgroundColor = "rgb(255, 255, 255)";
-}
-
-function out4() {
-    document.getElementsByClassName("s_c_list_li")[4].getElementsByTagName("ul")[0].style.left = "0px";
-    document.getElementsByClassName("s_c_list_li")[4].getElementsByTagName("ul")[0].style.display = "none";
-    document.getElementsByClassName("s_c_list_li")[4].style.backgroundColor = "rgb(255, 255, 255)";
-}
-
-function out5() {
-    document.getElementsByClassName("s_c_list_li")[5].getElementsByTagName("ul")[0].style.left = "0px";
-    document.getElementsByClassName("s_c_list_li")[5].getElementsByTagName("ul")[0].style.display = "none";
-    document.getElementsByClassName("s_c_list_li")[5].style.backgroundColor = "rgb(255, 255, 255)";
-}
+// function c0() {
+//     for(var cnt = 0; cnt < 4; cnt++) {
+//         var id = "s_click"+cnt;
+//         if(cnt == 0) {
+//             document.getElementById(id).style.color = "rgb(95, 0, 128)";
+//         } else {
+//             document.getElementById(id).style.color = "black";
+//         }
+//     }
+// }
+// function c1() {
+//     for(var cnt = 0; cnt < 4; cnt++) {
+//         var id = "s_click"+cnt;
+//         if(cnt == 1) {
+//             document.getElementById(id).style.color = "rgb(95, 0, 128)";
+//         } else {
+//             document.getElementById(id).style.color = "black";
+//         }
+//     }
+// }
+// function c2() {
+//     for(var cnt = 0; cnt < 4; cnt++) {
+//         var id = "s_click"+cnt;
+//         if(cnt == 2) {
+//             document.getElementById(id).style.color = "rgb(95, 0, 128)";
+//         } else {
+//             document.getElementById(id).style.color = "black";
+//         }
+//     }
+// }
+// function c3() {
+//     for(var cnt = 0; cnt < 4; cnt++) {
+//         var id = "s_click"+cnt;
+//         if(cnt == 3) {
+//             document.getElementById(id).style.color = "rgb(95, 0, 128)";
+//         } else {
+//             document.getElementById(id).style.color = "black";
+//         }
+//     }
+// }
 
