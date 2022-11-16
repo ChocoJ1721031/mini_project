23699d45e65b896d41c29ed48083d234d3a9cd23
MinJiPark <pminji1@gmail.com>
2022-11-16 16:35:47 +0900

    category_header.js 구현
    category_header.js 노드 영역에 마우스를 올려놓았을 때 색 변경
    클릭했을 때 수정해야 함.

---
+++ mini_project_박민지/js/category_header.js
@@ -0,0 +1,35 @@
+// category_header box 노드 영역 색 변경
+// click 사용
+console.log("확인하기");
+$(function() {
+    $('.cat_header_link').click(function() {
+        $('.cat_header_link').css("color", "black");
+        $(this).css("color", "rgb(95, 0, 128)");
+    })
+});
+
+
+
+// test
+
+// mouseover, mouseout 사용
+// $(document).ready(function(){
+//     $(".cat_header_link").mouseover(function(){
+//         $(this).css("color", "rgb(95, 0, 128)");
+//     })
+//     $(".cat_header_link").mouseout(function(){
+//         $('.cat_header_link').css("color", "black");
+//     })
+// });
+
+// click, if 사용
+// $(function() {
+//     $('.cat_header_link').click(function() {
+//         if($(this).css("color", "black"));{
+//             $(this).css("color", "rgb(95, 0, 128)");
+//         } else {
+//             $('.cat_header_link').css("color", "black");
+//         }              
+//     })
+// });
+
