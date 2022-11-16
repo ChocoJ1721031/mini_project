60cb235438521177c24d3b07f1c4990502001655
homin0203 <kwonhomin317@gmail.com>
2022-11-16 16:25:31 +0900

    header event배너 수정
    2022.11.16
    header 최상단 event배너 클릭 시 위로 올라가며 없어지는 기능 추가

--- mini_project_권호민/js/main_header.js
+++ mini_project_권호민/js/main_header.js
@@ -2,5 +2,8 @@ $(function() {
     $('.s_click').click(function() {
         $('.s_click').css("color", "black");
         $(this).css("color", "rgb(95, 0, 128)");
-    })
+    });
+    $('#event_btn').click(function() {
+        $('#event').css("margin-top", "-44px");
+    });
 });
